341f3fffb40e4e94ce91af76cf9333f820064a3f
buecherregale <david.grimmen@gmail.com>
2026-03-09 09:23:48 +0100

    feat: android: enable image generation on android

--- composeApp/src/androidMain/kotlin/dev/buecherregale/ebook_reader/ui/util/ImageUtil.android.kt
+++ composeApp/src/androidMain/kotlin/dev/buecherregale/ebook_reader/ui/util/ImageUtil.android.kt
@@ -1,7 +1,12 @@
 package dev.buecherregale.ebook_reader.ui.util
 
+import android.graphics.Bitmap
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asAndroidBitmap
+import java.io.ByteArrayOutputStream
 
 internal actual fun ImageBitmap.encodeToByteArray(): ByteArray {
-    TODO("Not yet implemented")
+    val stream = ByteArrayOutputStream()
+    asAndroidBitmap().compress(Bitmap.CompressFormat.PNG, 100, stream)
+    return stream.toByteArray()
 }
